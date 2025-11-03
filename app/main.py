6694e802ebfaffc8bc0da47c4221811069195be1
tomg2031 <tomg2031@gmail.com>
2025-11-03 15:10:53 -0600

    [any message]

--- app/main.py
+++ app/main.py
@@ -13,10 +13,13 @@ def errorMessage():
     command = input()
     if command.startswith("exit"): 
         sys.exit(0)
-    
+    command_list = command.split(" ")
+    if command_list[0] == "echo":
+        output = " ".join(command_list[1:] + "\n")
+        sys.stdout.write(output)
     else:
         # prints the "<command>: command not found" message
-        print(f"{command}: command not found")
+        print(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
