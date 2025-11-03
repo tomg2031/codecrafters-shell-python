29d5eb17c5297808678f464adac99829fe6e6e84
tomg2031 <tomg2031@gmail.com>
2025-11-03 14:47:12 -0600

    [any message]

--- app/main.py
+++ app/main.py
@@ -4,7 +4,11 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
+    errorMessage()
+    main()
+
     
+def errorMessage():
     # Wait for user input
     command = input()
     print(f"{command}: command not found")
