41338243529e4e37b706c1b7406ca404e816504c
tomg2031 <tomg2031@gmail.com>
2025-11-03 05:31:05 -0600

    [any message]

--- app/main.py
+++ app/main.py
@@ -4,7 +4,10 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
-    pass
+    
+    # Wait for user input
+    command = input()
+    print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
